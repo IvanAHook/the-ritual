53c54c709ea45fa75399839cac8a8677591be1c2
Ivan Höök <ivan.a.hook@gmail.com>
2014-01-14 14:44:35 +0100

    restricted character velocity and rotation

--- character.lua
+++ character.lua
@@ -2,29 +2,45 @@
 character = {}
 
 function character.load()
-    jump = false
-    -- is this a good model?
-    -- character.properties.x_velocity = 0
-    -- character.properties.y_velocity = 0
-    -- character.state = ""
+    character.grounded = false
+    character.state = "alive"
 end
 
 function character.update(dt)
+    velX, velY = character.body:getLinearVelocity()
     if love.keyboard.isDown("d") then -- how do i handle movement in a neat way that also feels good?
-        game.character.body:applyForce(100, 0)
+        character.body:applyForce(100, 0)
     end
     if love.keyboard.isDown("a") then
-        game.character.body:applyForce(-100, 0)
+        character.body:applyForce(-100, 0)
+    end
+    if velX > 200 then
+        character.body:setLinearVelocity(200, velY)
+    end
+    if velX < -200 then
+        character.body:setLinearVelocity(-200, velY)
     end
 end
 
-function character.draw()
+function character.keypressed(key)
+    if key == "j" and character.grounded == true then
+        character.body:applyLinearImpulse(0, -30) -- character.jump or something in that manner
+        character.grounded = false
+    end
 end
 
-function character.keypressed()
-
+function character.keyreleased(key)
+   if (key == "a" or  key == "d") and character.grounded == true then
+         character.body:setLinearVelocity(0, 0)
+    end
 end
 
 function character.jump()
 
+end
+
+function character.draw()
+    love.graphics.setColor(193, 47, 14)
+    love.graphics.polygon("fill", character.body:getWorldPoints(
+                                    character.shape:getPoints()))
 end
--- game.lua
+++ game.lua
@@ -5,13 +5,13 @@ game = {}
 
 function game.load()
     text = ""
-    grounded = true
     size = 20
     love.physics.setMeter(64)
+
     world = love.physics.newWorld(0, 9.81*64, true)
     world:setCallbacks(beginContact, endContact)
 
-    game.objects = {}
+    game.objects = {} -- change to world.objects = {} and npc = {}
 
     game.objects.ground = {}
     game.objects.ground.body = love.physics.newBody(world, 800/2, 650-50/2, "static") -- static is default, typed for clarity
@@ -55,52 +55,39 @@ function game.load()
     game.objects.box.fixture:setRestitution(0)
     game.objects.box.fixture:setUserData("box")
 
-    game.character = charater.load()
-    -- require('character') and add init function to make this possible?
-    -- game.character.properties = character:init()
-    game.character.body = love.physics.newBody(world, 500-20/2, 500-20/2, "dynamic")
-    game.character.shape = love.physics.newRectangleShape(20, 20)
-    game.character.fixture = love.physics.newFixture(
-                                        game.character.body,
-                                        game.character.shape)
-    game.character.fixture:setRestitution(0.0)
-    -- game.character.fixture:setFriction(4.0)
-    game.character.fixture:setUserData("char")
+    character.load()
+    character.body = love.physics.newBody(world, 500-20/2, 500-20/2, "dynamic")
+    character.shape = love.physics.newRectangleShape(20, 20)
+    character.fixture = love.physics.newFixture(
+                                        character.body,
+                                        character.shape)
+    character.fixture:setRestitution(0.0)
+    character.fixture:setUserData("char")
+    character.body:setFixedRotation(true)
 end
 
 function game.update(dt)
     world:update(dt) --this puts the world into motion
-    game.character.update()
-    if love.keyboard.isDown("d") then -- how do i handle movement in a neat way that also feels good?
-        game.character.body:applyForce(100, 0)
-    end
-    if love.keyboard.isDown("a") then
-        game.character.body:applyForce(-100, 0)
-    end
+    character.update(dt)
     if string.len(text) > 500 then
         text = ""
     end
+    text = character.body:getLinearVelocity()
 end
 
 function game.keypressed(key)
-    -- game.character:jump() -- make function in character, how would this work?
-    if key == "j" and grounded == true then
-        game.character.body:applyLinearImpulse(0, -30) -- *game.character.properties.jump or something in that manner
-        grounded = false
-    end
+    character.keypressed(key)
 end
 
 function game.keyreleased(key)
-   if (key == "a" or  key == "d") and grounded == true then
-         game.character.body:setLinearVelocity(0, 0)
-    end
+    character.keyreleased(key)
 end
 
 function beginContact(a, b, coll)
     local x, y = coll:getNormal()
-    text = text .. "\n" .. x .. ", " .. y .. " " .. a:getUserData() .. " " .. b:getUserData()
-    if b:getUserData() == "char" and a:getBody():getY() > b:getBody():getY() then
-        grounded = true
+--    text = text .. "\n" .. x .. ", " .. y .. " " .. a:getUserData() .. " " .. b:getUserData()
+    if b:getUserData() == "char" and a:getBody():getY() > b:getBody():getY() then -- this requires character to be added last in game.load
+        character.grounded = true
     end
 end
 
@@ -112,8 +99,7 @@ function game.set_level_objects(objects)
 end
 
 function game.draw()
-    -- for each in game.objects do
-    love.graphics.setColor(72, 160, 14) -- set the drawing color to green for the ground
+    love.graphics.setColor(72, 160, 14)
     love.graphics.polygon("fill", game.objects.ground.body:getWorldPoints(
                                     game.objects.ground.shape:getPoints()))
     love.graphics.polygon("fill", game.objects.l_wall.body:getWorldPoints(
@@ -123,16 +109,16 @@ function game.draw()
     love.graphics.polygon("fill", game.objects.roof.body:getWorldPoints(
                                     game.objects.roof.shape:getPoints()))
 
-    love.graphics.setColor(193, 47, 14) --set the drawing color to red for the ball
-    love.graphics.polygon("fill", game.character.body:getWorldPoints(
-                                    game.character.shape:getPoints()))
+    love.graphics.setColor(0, 0, 0, 150)
+    love.graphics.rectangle("fill", 0, 0, 400, 400 )
+
+    character.draw()
+
     love.graphics.setColor(193, 147, 14)
     love.graphics.polygon("fill", game.objects.box.body:getWorldPoints(
                                     game.objects.box.shape:getPoints()))
---    love.graphics.rectangle("fill", game.character.body:getX()-10,
---                                    game.character.body:getY()-10,
---                                    20, 20)
-    love.graphics.setColor(0, 0, 0)
+
+    love.graphics.setColor(255, 255, 255)
     love.graphics.print(text, 10, 10)
 end
 
