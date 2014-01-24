b102e87e6553e56bdcd470413a12bb8ebc280692
Ivan Höök <ivan.a.hook@gmail.com>
2014-01-24 09:31:45 +0100

    direction added

--- unit.lua
+++ unit.lua
@@ -7,6 +7,27 @@ function unit:spawn(world, x, y, object_type, userdata)
     self.fixture:setRestitution(0.0)
     self.fixture:setUserData(userdata)
     self.body:setFixedRotation(true)
+
+    self.lastx, self.lasty = x, y
+end
+
+function unit:getDirection() -- not working
+    if self.body then
+        local x, y = self.body:getPosition()
+        local direction
+        if x > self.lastx then
+            direction = 1
+            print(1)
+        elseif x < self.lastx then
+            direction = -1
+            print(2)
+        elseif x == self.lastx then
+            direction = 0
+            print(3)
+        end
+        self.lastx, self.lasty = x, y
+        return direction
+    end
 end
 
 function unit:draw()
